d95d3d294c0b0cdfba7791cfccaa74862f83daf8
Cameron Grieselding <cameron@Camerons-MacBook-Pro.local>
2021-08-05 16:29:12 -0400

    update push on Aug5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseURL = "https://ergast.com/api/f1/2021/drivers.json";
+const baseURL = "http://ergast.com/api/f1/2021/driverStandings.json";
 const results = document.getElementById("results");
 
 function fetchDrivers() {
@@ -8,7 +8,7 @@ function fetchDrivers() {
       console.log(data);
       displayDrivers(data);
     });
-}
+};
 
 fetchDrivers();
 
@@ -16,7 +16,7 @@ let row = document.createElement("div");
 row.setAttribute("class", "row");
 
 function displayDrivers(data) {
-  for (driver of data.MRData.DriverTable.Drivers) {
+  for (driver of data.MRData.StandingsTable.StandingsLists[0].DriverStandings) {
     console.log(driver);
 
     let col = document.createElement("div");
@@ -24,15 +24,15 @@ function displayDrivers(data) {
 
     let button = document.createElement("a");
     button.setAttribute("data-bs-toggle", "modal");
-    button.setAttribute("data-bs-target", `#${driver.givenName}`);
+    button.setAttribute("data-bs-target", `#${driver.Driver.givenName}`);
 
-    button.innerText = driver.givenName + " " + driver.familyName;
+    button.innerText = driver.Driver.givenName + " " + driver.Driver.familyName;
 
     let modal = document.createElement("div");
     modal.setAttribute("class", "modal fade");
-    modal.setAttribute("id", `${driver.givenName}`);
+    modal.setAttribute("id", `${driver.Driver.givenName}`);
     modal.setAttribute("tabindex", -1);
-    modal.setAttribute("aria-labelledby", `${driver.givenName}ModalLabel`);
+    modal.setAttribute("aria-labelledby", `${driver.Driver.givenName}ModalLabel`);
     modal.setAttribute("aria-hidden", true);
 
     let modalDialog = document.createElement("div");
@@ -49,26 +49,37 @@ function displayDrivers(data) {
     buttonTwo.setAttribute("data-bs-dismiss", "modal");
     buttonTwo.setAttribute("aria-label", "Close");
 
-    let nameHeader = document.createElement("h5");
-    nameHeader.innerText = driver.givenName + " " + driver.familyName;
+    let nameHeader = document.createElement("h2");
+    nameHeader.innerText = driver.Driver.givenName + " " + driver.Driver.familyName;
 
     let modalBody = document.createElement("div");
     modalBody.setAttribute("class", "modal-body");
 
     let birthDate = document.createElement("p");   
-    birthDate.innerText = 'Date of Birth: ' + driver.dateOfBirth;
+    birthDate.innerText = 'Date of Birth (YYYY/MM/DD): ' + driver.Driver.dateOfBirth;
 
     let driverNationality = document.createElement("p");
-    driverNationality.innerText = 'Nationality: ' + driver.nationality;
+    driverNationality.innerText = 'Nationality: ' + driver.Driver.nationality;
 
     let driverNumber = document.createElement("p");
-    driverNumber.innerText = 'Car Number: ' + driver.permanentNumber;
+    driverNumber.innerText = 'Car Number: ' + driver.Driver.permanentNumber;
+
+    let driverRank = document.createElement("p");
+    driverRank.innerText = 'Current Rank This Season: ' + driver.position;
+
+    let driverPoints = document.createElement("p");
+    driverPoints.innerText = 'Total Points This Season: ' + driver.points;
+
+    let driverWins = document.createElement("p");
+    driverWins.innerText = 'Total Wins This Season: ' + driver.wins;
 
     let driverBiography = document.createElement("a");
-    let biographyText = document.createTextNode('Click Here for Driver\'s Biography')
+    driverBiography.setAttribute('target', '_blank');
+    let biographyText = document.createTextNode(`Read More About ${driver.Driver.givenName + " " + driver.Driver.familyName} Here!`)
     driverBiography.appendChild(biographyText);
-    driverBiography.href = driver.url
-    driverBiography.style.color = "#ff1801"
+    driverBiography.href = driver.Driver.url;
+    window.open(driverBiography.href, '_blank');
+    driverBiography.style.color = "#ff1801";
 
     let modalFooter = document.createElement("div");
     modalFooter.setAttribute("class", "modal-footer");
@@ -87,9 +98,10 @@ function displayDrivers(data) {
     modalBody.appendChild(birthDate);
     modalBody.appendChild(driverNationality); 
     modalBody.appendChild(driverNumber);
+    modalBody.appendChild(driverRank);
+    modalBody.appendChild(driverPoints);
+    modalBody.appendChild(driverWins);
     modalBody.appendChild(driverBiography);
-    modalContent.appendChild(modalFooter)
+    modalContent.appendChild(modalFooter);
   }
 }
-
-// Adding images in???
